6bd133df30fde2854651526d73191f78e989341e
João Vitor Fernandes <joaovitorcarvalhofernandes@hotmail.com>
2022-06-29 14:04:29 -0300

    Finalização

--- HTML/The Global Goals/assets/js/script.js
+++ HTML/The Global Goals/assets/js/script.js
@@ -2,6 +2,12 @@ let nome = document.querySelector('#nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
 
+let nomeOK = false
+let emailOK = false
+let assuntoOK = false
+
+let mapa = document.querySelector('#mapa')
+
 function validarNome() {
     let txtNome = document.querySelector('#txtNome')
     if(nome.value.length < 3 ) {
@@ -10,6 +16,7 @@ function validarNome() {
     } else {
         txtNome.innerHTML = 'Nome válido!'
         txtNome.style.color = 'green'
+        nomeOK = true
     }
 }
 
@@ -21,13 +28,35 @@ function validarEmail() {
     } else {
         txtEmail.innerHTML = "Email válido!"
         txtEmail.style.color = 'green'
+        emailOK = true
     }
 }
 
 function validarAssunto() {
     let txtAssunto = document.querySelector('#txtAssunto')
     if(assunto.value.length > 100) {
-        txtAssunto.innerHTML = 'O número máximo de caracteres é de 100.'
+        txtAssunto.innerHTML = 'O texto é muito grande, digite no máximo 100 caracteres!.'
         txtAssunto.style.color = 'red'
-    } 
+    } else {
+        assuntoOK = true
+    }
+}
+
+function enviar() {
+
+    if (assuntoOK == true && nomeOK == true && emailOK == true) {
+        alert('Formulário enviado com sucesso!')
+    } else {
+        alert('Preencha o formulário corretamente!')
+    }
+}
+
+function mapaZoom() {
+    mapa.style.width = '800px'
+    mapa.style.hegth = '600px'
+    }
+
+function mapaNormal() {
+    mapa.style.width = '800px'
+    mapa.style.hegth = '200px'
 }
